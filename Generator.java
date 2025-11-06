a6aafcf4556dbb89df87ed74d8da0d04ee624c7e
Notapplicable-ux <anandpopo91@gmail.com>
2025-11-06 19:18:32 +0000

    Iphone

--- Generator.java
+++ Generator.java
@@ -1,1 +1,26 @@
+//Name: Anand Raj
+//Date: 10/30/2025
+//This programs draws shapes using loops
 
+public class Generator {
+    public static void stars(int rows){
+
+        while(rows > 0){
+            
+            System.out.println("*");
+            rows--;
+        }
+    }
+    public static void triangle(int rows) {
+        
+    }
+    public static void odds(int rows) {
+        
+    }
+    public static void even(int rows) {
+        
+    }
+    public static void pyramid(int rows) {
+        
+    }
+}
